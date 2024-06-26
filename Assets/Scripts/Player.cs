788818d75ab770941606b86e5f15e193fcf39c4d
Nico <oprescunico@gmail.com>
2024-06-26 22:47:34 +0100

    Added Basic NPC Interactions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Player : MonoBehaviour
 {
+    private bool inInteraction = false;
+    private string prompt;
+
+    public TMP_Text PromptText;
+    public GameObject PromptObject;
+    string CollidingObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +26,45 @@ public class Player : MonoBehaviour
         int speed = 5;
 
         if(Input.GetKey(KeyCode.D))
-                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                transform.Translate(speed * Time.deltaTime * UnityEngine.Vector2.right);
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            transform.Translate(speed * Time.deltaTime * UnityEngine.Vector2.left);
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector2.up * speed * Time.deltaTime);
+            transform.Translate(speed * Time.deltaTime * UnityEngine.Vector2.up);
         if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector2.down * speed * Time.deltaTime);
+            transform.Translate(speed * Time.deltaTime * UnityEngine.Vector2.down);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inInteraction == false)
+            {
+                inInteraction = true;
+
+                PromptText.text = prompt;
+                PromptObject.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+            else
+            {
+                inInteraction = false;
+                PromptObject.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        Debug.Log("Hi");
+        CollidingObject = col.gameObject.name;
+        
+        prompt = CollidingObject switch
+        {
+            "NPC" => "Hi! You must be new here!",
+            _ => "Hmm... Nothing to see here!",
+        };
+
+        
     }
 }
