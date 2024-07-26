a82856600b94cfa7f281ea21f3401ab2d80dfadc
Nico <oprescunico@gmail.com>
2024-07-26 02:28:04 +0100

    Implemented Robot Health and Strength Management

--- Assets/Scripts/Characters/Robot.cs
+++ Assets/Scripts/Characters/Robot.cs
@@ -5,27 +5,48 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     private float Health;
-    private float Stamina;
+    private float Strength;
 
     // Start is called before the first frame update
     void Start()
     {
         Health = 150f;
-        Stamina = 250f;
+        Strength = 250f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Strength < 250) RegenerateStrength();
     }
 
-    public float ReturnStamina()
+    // Getters;
+    public float GetStrength()
     {
-        return Stamina;
+        return Strength;
     }
-    public float ReturnHealth()
+    public float GetHealth()
     {
         return Health;
+    }
+
+    // Functions relating to the health of the robot.
+    public void TakeDamage(float Damage)
+    {
+        Health -= Damage;
+    }
+    public void IncreaseHealth()
+    {
+        Health += Health + 50f;
+    }
+
+    // Functions relating to the strength of the robot.
+    private void DecreaseStrength(float AttackPower)
+    {
+        Strength -= AttackPower;
+    }
+    private void RegenerateStrength()
+    {
+        Strength = (Strength + (Strength + 5) / 2.0f) % 250;
     }
 }
