921019180e6794bb2e747c031b7ff2077bad68c6
Nico <oprescunico@gmail.com>
2024-08-28 03:08:47 +0100

    Began Implementation of All Enemies

--- Assets/Scripts/Characters/Enemies - Bosses/SecondBoss.cs
+++ Assets/Scripts/Characters/Enemies - Bosses/SecondBoss.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SecondBoss : MonoBehaviour
+public class SecondBoss : Enemy
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        AttackCooldown = 3f;
+        Health = 500f;
+
+        CurrentState = State.Idle;
+        Player = GameObject.Find("Robot");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (CurrentState)
+        {
+            case State.Idle:
+                break;
+            case State.Attacking:
+                Attack();
+                break;
+            case State.TakingDamage:
+                break;
+            case State.Moving:
+                MoveToPlayer();
+                break;
+            case State.Dead:
+                break;
+        }
+    }    
+    
+    public override void Attack()
+    {
+
+    }
+
+    public override void MoveToPlayer()
+    {
+
+    }
+
+    public override void TakeDamage(float Damage)
+    {
+        Health -= Damage;
+        if (Health < 0) ChangeState(State.Dead);
     }
 }
--- Assets/Scripts/Characters/Enemies - Bosses/ThirdBoss.cs
+++ Assets/Scripts/Characters/Enemies - Bosses/ThirdBoss.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdBoss : MonoBehaviour
+public class ThirdBoss : Enemy
 {
+    private int ChanceOfHeavyAttack = 35;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AttackCooldown = 2f;
+        Health = 1000f;
+
+        CurrentState = State.Idle;
+        Player = GameObject.Find("Robot");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (CurrentState)
+        {
+            case State.Idle:
+                break;
+            case State.Attacking:
+                Attack();
+                break;
+            case State.TakingDamage:
+                break;
+            case State.Moving:
+                MoveToPlayer();
+                break;
+            case State.Dead:
+                break;
+        }
+    }    
+    
+    public override void Attack()
+    {
+
+    }
+
+    public override void MoveToPlayer()
+    {
+
+    }
+
+    public override void TakeDamage(float Damage)
+    {
+        Health -= Damage;
+
+        if (Health < 0) ChangeState(State.Dead);
+        if (Health < 500) ChanceOfHeavyAttack = 45;
     }
 }
