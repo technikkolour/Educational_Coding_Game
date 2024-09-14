e616050fec5a117024618313c9abc9f33c5f9629
Nico <oprescunico@gmail.com>
2024-09-14 04:41:52 +0100

    Added Tiling to First Level

--- Assets/Scripts/Player Functions/Journal.cs
+++ Assets/Scripts/Player Functions/Journal.cs
@@ -15,7 +15,7 @@ public class Journal : MonoBehaviour
         {2, new(){ "Integer", "Used to store whole numbers, like the number of people present in a room." } },
         {3, new(){ "Float", "Used to store decimals, such as your height." } },
         {4, new(){ "Boolean", "Used to store true or false values." } },
-        {5, new(){ "List", "They can store multiple values of the same type together. For example, they can be used for storing a student's grades (floats) or a list of the students enrolled in a class (strings)." } },
+        {5, new(){ "Arrays", "They can store multiple values of the same type together. For example, they can be used for storing a student's grades (floats) or a list of the students enrolled in a class (strings)." } },
         {6, new(){ "If Statements", "Used to perform some actions based on one or more conditions." } },
         {7, new(){ "For", "A set of actions is performed for every time a value is updated. The starting and final values, as well as the step size, are defined." } },
         {8, new(){ "While", "A set of actions is performed as long as a condition is true." } },
